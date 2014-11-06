b03351d7e35ac42989458bbab4ab0a780c0d415e
Anuradha Gunasekara <theanuradha@gmail.com>
2014-11-06 14:35:19 +0530

    re Enable Warp

--- entirej-reports-jasper/src/org/entirej/report/jasper/EJJasperReports.java
+++ entirej-reports-jasper/src/org/entirej/report/jasper/EJJasperReports.java
@@ -363,7 +363,7 @@ public class EJJasperReports
                     configuration.setWhitePageBackground(Boolean.FALSE);
                     configuration.setRemoveEmptySpaceBetweenRows(Boolean.TRUE);
                     configuration.setRemoveEmptySpaceBetweenColumns(Boolean.TRUE);
-                    configuration.setWrapText(false);
+                    configuration.setWrapText(true);
                     configuration.setIgnorePageMargins(true);
                     exporter.setConfiguration(configuration);
                     exporter.exportReport();
@@ -379,7 +379,7 @@ public class EJJasperReports
                     exporter.setExporterInput(new SimpleExporterInput(print));
                     exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(destFile));
                     SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
-                    configuration.setWrapText(false);
+                    configuration.setWrapText(true);
                     configuration.setOnePagePerSheet(false);
                     configuration.setIgnorePageMargins(true);
                     configuration.setDetectCellType(Boolean.TRUE);
