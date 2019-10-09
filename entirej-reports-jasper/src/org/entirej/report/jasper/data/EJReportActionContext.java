47c0a2f14c68ccc788e04dbdd84e0e8739a33584
Anuradha Gunasekara <theanuradha@gmail.com>
2019-10-09 12:10:20 +0530

    cleanup

--- entirej-reports-jasper/src/org/entirej/report/jasper/data/EJReportActionContext.java
+++
@@ -1,9 +0,0 @@
-package org.entirej.report.jasper.data;
-
-public interface EJReportActionContext
-{
-    public boolean canShowBlock(String blockName);
-
-    public boolean canShowScreenItem(String blockName, String screenItem, String section);
-
-}
